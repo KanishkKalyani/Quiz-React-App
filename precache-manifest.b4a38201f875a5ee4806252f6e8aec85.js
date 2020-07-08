0ec64dda1813f6b33f84ff2ac6c4fd9a8b246dd6
KanishkKalyani <kanishkkalyani@gmail.com>
2020-07-09 01:09:52 +0530

    Updates

---
+++ precache-manifest.b4a38201f875a5ee4806252f6e8aec85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1ce6df8724b69deeba5680b409c307",
+    "url": "/Quiz-React-App/index.html"
+  },
+  {
+    "revision": "7a7d6b047dae760c91b2",
+    "url": "/Quiz-React-App/static/css/main.b66eef81.chunk.css"
+  },
+  {
+    "revision": "9a396872dc32bdcbbcc8",
+    "url": "/Quiz-React-App/static/js/2.c3215de1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-React-App/static/js/2.c3215de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7d6b047dae760c91b2",
+    "url": "/Quiz-React-App/static/js/main.69bead06.chunk.js"
+  },
+  {
+    "revision": "8d186bb41e056f05d9fc",
+    "url": "/Quiz-React-App/static/js/runtime-main.f72db49f.js"
+  }
+]);
